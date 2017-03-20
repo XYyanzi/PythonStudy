4d988a10ace69abcfad87290c6ebf6886c0246ae
XYyanzi <yan_babyyu@126.com>
2017-03-20 18:31:42 +0800

    增加另外一种实现九九乘法表的方法

--- Python01/生成99乘法表到excel文件中.py
+++ Python01/生成99乘法表到excel文件中.py
@@ -1,8 +1,7 @@
 # encoding: utf-8
-
+from openpyxl import Workbook
 __author__ = 'xunyan'
 
-from openpyxl import Workbook
 
 # 在内存中创建一个workbook对象，而且会至少创建一个 worksheet
 wb = Workbook()
@@ -21,13 +20,19 @@ ws.cell(row=3, column=1).value = 6
 
 # 新打开一个sheet页，sheet页的标题命名为
 new_ws = wb.create_sheet(title=u'乘法表')
+# row = 1
+# col = 2
+# new_ws.cell(row=1, column=1).value = str(row) + "*" + str(col) + "=" + str(row*col)
 
 # 向sheet页中写入值
 for row in range(1, 10):
-    for col in range(1, 10):
-        new_ws.cell(row=row, column=col).value = row * col
+    for col in range(1, row+1):
+        new_ws.cell(row=row, column=col).value = str(row) + "*" + str(col) + "=" + str(row*col)
+
+# 简单实现方法
+# for row in range(1, 10):
+#     for col in range(1, 10):
+#         sheet1.cell(row=row, column=col).value = row * col
+
 # 最后一定要保存！
 wb.save(filename='write.xlsx')
-
-
-
